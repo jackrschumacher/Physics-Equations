9386aa0a047bb5a803488448d8b654cf31627356
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-10-24 15:58:29 +0000

    Add Documentation

--- Physics.java
+++ Physics.java
@@ -5,4 +5,17 @@ public class Physics{
   /*xf = xi + vi * t + 1/2 * a * t^2
   *final position = initial position position + the product of initial speed + product of 1/2 and acceration and time squared
   */
+
+  /* Position after a constant acceleration
+  *using the above information, we know all of the information except final position
+  *Precondition: double valyes are supplied for xi, vi, t and a
+  *Postondition: xf is returned as a double
+  */
+  public static double positionConstantAccelMissingFinalPosition(
+    double xi,
+    double vi,
+    double t,
+    double a){
+    
+    }
 }
