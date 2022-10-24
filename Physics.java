21d495598693264d1fed7184934f636e4312d923
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-10-24 15:54:42 +0000

    Add Documentation

--- Physics.java
+++ Physics.java
@@ -1,3 +1,8 @@
 public class Physics{
-  private static final double Gravity = -9.81;
+  private static final double GRAVITY = -9.81; //Upercase bcause constant (naming convention)
+
+   
+  /*xf = xi + vi * t + 1/2 * a * t^2
+  *final position = initial position position + the product of initial speed + product of 1/2 and acceration and time squared
+  */
 }
